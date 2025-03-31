bccb626440eaa7a91b242e698e34cc38aebeafe1
1102csh <121600882+1102csh@users.noreply.github.com>
2025-03-31 22:32:43 +0900

    Update authRoutes.js

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const AuthController = require("../controllers/authController");
-
 const router = express.Router();
+const AuthController = require("../controllers/authController");
 
 router.post("/register", AuthController.register);
 router.post("/login", AuthController.login);
+router.post("/logout", AuthController.logout);
+router.get("/me", AuthController.getMe);
 
 module.exports = router;
